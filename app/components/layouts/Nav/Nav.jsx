0a58006decfc7ef058b1d51263178c4bc4f7f2d4
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-03-10 15:09:16 +0545

    feat & style: Link component from Next imported and icons styled

--- app/components/layouts/Nav/Nav.jsx
+++ app/components/layouts/Nav/Nav.jsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 import {
   MdSearch,
   MdOutlineShoppingCart,
@@ -9,33 +10,53 @@ import { CgMenuHotdog } from "react-icons/cg";
 import styles from "./Nav.module.scss";
 
 const Nav = () => {
+  
+  const iconStyle = {
+    color: "#2A333E",
+    fontSize: "1.5rem",
+    cursor: "pointer",
+    _hover: {
+      color: "#000000",
+    }
+  }
+
   return (
     <>
       <header className={styles.header}>
         <div className={styles.header__left}>
-          <Image src="/images/logo.svg" alt="Logo" height={30} width={100} />
+          <Link href="/" passHref>
+            <Image src="/images/logo.svg" alt="Logo" height={30} width={100} />
+          </Link>
         </div>
         <nav className={styles.header__center}>
           <ul>
             <li>
-              <a href="#">Menu</a>
-              <a href="#">About Us</a>
-              <a href="#">Our Specials</a>
-              <a href="#">Our Locations</a>
-              <a href="#">Our Chef</a>
+              {/* {linkItems.map(item => {
+                <a
+                  key={item}
+                  href={`/#${item.replace(/\s+/g, "").toLowerCase()}`}
+                >
+                  {item}
+                </a>;
+              })} */}
+              <Link href="#menu" passHref>Menu</Link>
+              <Link href="#about" passHref>About Us</Link>
+              <Link href="#specials" passHref>Our Specials</Link>
+              <Link href="#locations" passHref>Our Locations</Link>
+              <Link href="#chef" passHref>Our Chef</Link>
             </li>
           </ul>
         </nav>
         <div className={styles.header__right}>
-          <MdSearch />
-          <MdOutlineShoppingCart />
-          <MdOutlineAccountCircle />
+          <MdSearch style={iconStyle} />
+          <MdOutlineShoppingCart style={iconStyle} />
+          <MdOutlineAccountCircle style={iconStyle} />
           <div className={styles.header__menu}>
             <div className={styles.header__menu__open}>
-              <CgMenuHotdog />
+              <CgMenuHotdog style={iconStyle} />
             </div>
             <div className={styles.header__menu__close}>
-              <MdRestaurantMenu />
+              <MdRestaurantMenu style={iconStyle} />
             </div>
           </div>
         </div>

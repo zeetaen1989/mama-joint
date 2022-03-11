668bc57ec05b4c6c332c71b86b32265969b04cb7
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-03-11 15:48:46 +0545

    feat(component): new component created

---
+++ app/components/layouts/PopularProducts/PopularProducts.jsx
@@ -0,0 +1,13 @@
+import  styles from "./PopularProducts.module.scss";
+
+const PopularProducts = () => {
+    return (
+        <div className={styles.container}>
+            <div className={styles.subtitle}>
+                <h3>online store</h3>
+            </div>
+        </div>
+    )
+}
+
+export default PopularProducts

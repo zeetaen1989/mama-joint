97ef6bad6ce79e15318ed49d3c6eb5454fccfe99
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-03-11 12:54:53 +0545

    refactor(pages & mixins)

--- pages/index.js
+++ pages/index.js
@@ -2,23 +2,27 @@ import Head from "next/head";
 import { Nav, Footer, Hero } from "../app/components/layouts";
 
 const Home = () => {
+
+	const navStyle = {
+		position: "relative",
+		zIndex: "999",
+	}
+
+	const heroStyle = {
+		position: "absolute",
+		zIndex: "1",
+		top: "1rem",
+	}
+
 	return (
 		<>
 			<Head>
 				<title>FoodLab | Pizza Delivery Services</title>
-				<meta
-					name="description"
-					content="This is a pizza delivery service application."
-				/>
-				<meta name="viewport" content="initial-scale=1.0, width=device-width" />
-				<meta name="theme-color" content="#000000" />
-				<meta author="Pizza Delivery Services" />
-				<link rel="icon" href="/favicon.ico" />
-			</Head>
-			<div className="nav" style={{ position: "relative", zIndex: "999" }}>
+z			</Head>
+			<div className="nav" style={navStyle}>
 				<Nav />
 			</div>
-			<div className="hero" style={{ position: "absolute", zIndex: "1" }}>
+			<div className="hero" style={heroStyle}>
 				<Hero />
 			</div>
 			<Footer />

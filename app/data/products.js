79a1b5d6f93477e2b4a53b2e96e2604032bbde2a
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-03-11 21:16:43 +0545

    feat(images): products images added

---
+++ app/data/products.js
@@ -0,0 +1,26 @@
+export const products = [
+    {
+        id: 1,
+        title: "Hot Wings",
+        category: "Chinese",
+        img: "/images/hot-wings.png",
+    },
+    {
+        id: 2,
+        title: "Rice with Curry",
+        category: "Indian",
+        img: "/images/rice-with-curry.png",
+    },
+    {
+        id: 3,
+        title: "Chapatees",
+        category: "Pakistani",
+        img: "/images/chapatees.png",
+    },
+    {
+        id: 4,
+        title: "Sweetdish",
+        category: "Western",
+        img: "/images/sweetdish.png",
+    }
+]

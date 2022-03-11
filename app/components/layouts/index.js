ba89d76464beae82fda3a71a374957388c4c0a80
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-03-11 15:47:52 +0545

    feat(import): PopularProducts components imported

--- app/components/layouts/index.js
+++ app/components/layouts/index.js
@@ -1,3 +1,4 @@
 export { default as Nav } from "./Nav/Nav";
 export { default as Hero } from "./Hero/Hero";
+export { default as PopularProducts } from "./PopularProducts/PopularProducts";
 export { default as Footer } from "./Footer/Footer";

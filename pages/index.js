12005eb7f0ac3116d1e32a721787a8b0d994a103
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-03-11 15:48:25 +0545

    fix(position): each components position issue fixed

--- pages/index.js
+++ pages/index.js
@@ -1,33 +1,19 @@
 import Head from "next/head";
-import { Nav, Footer, Hero } from "../app/components/layouts";
+import { Nav, Hero, PopularProducts, Footer, } from "../app/components/layouts";
 
-const Home = () => {
-
-	const navStyle = {
-		position: "relative",
-		zIndex: "999",
-	}
-
-	const heroStyle = {
-		position: "absolute",
-		zIndex: "1",
-		top: "1rem",
-	}
+const LandingPage = () => {
 
 	return (
 		<>
 			<Head>
 				<title>FoodLab | Pizza Delivery Services</title>
-z			</Head>
-			<div className="nav" style={navStyle}>
-				<Nav />
-			</div>
-			<div className="hero" style={heroStyle}>
-				<Hero />
-			</div>
+			</Head>
+			<Nav />
+			<Hero />
+			<PopularProducts />
 			<Footer />
 		</>
 	);
 };
 
-export default Home;
+export default LandingPage;

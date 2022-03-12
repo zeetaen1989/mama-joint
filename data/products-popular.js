7bebc45119a4c036ad7a61f6da39eb3d6668c017
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-03-12 17:56:08 +0545

    feat(ratings): rating key added to the products data

--- data/products-popular.js
+++ data/products-popular.js
@@ -4,23 +4,27 @@ export const products = [
         title: "Hot Wings",
         category: "Chinese",
         img: "/images/hot-wings.png",
+        rating: 5,
     },
     {
         id: 2,
         title: "Rice with Curry",
         category: "Indian",
         img: "/images/rice-with-curry.png",
+        rating: 4.5,
     },
     {
         id: 3,
         title: "Chapatees",
         category: "Pakistani",
         img: "/images/chapatees.png",
+        rating: 3.5,
     },
     {
         id: 4,
         title: "Sweetdish",
         category: "Western",
         img: "/images/sweet-dish.png",
+        rating: 4,
     }
 ]

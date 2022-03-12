038afbc3c9de69de4cd765c99bf22e96de6de6f2
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-03-12 10:16:48 +0545

    feat(markup): products section markups added

--- app/components/layouts/PopularProducts/PopularProducts.jsx
+++ app/components/layouts/PopularProducts/PopularProducts.jsx
@@ -23,11 +23,26 @@ const PopularProducts = () => {
             </div>
             <div className={styles.container__products}>
                 {products.map(product => {
-                    <>
-                        <Image key={product.id} src={product.img} height={187} width={196} alt={product.title} />
-                        <p>{product.category}</p>
-                        <h2>{product.title}</h2>
-                    </>
+                    return (
+                        <div className={styles.products__list} key={product.id}>
+                            <Image
+                                key={product.id}
+                                src={product.img}
+                                height={200}
+                                width={210}
+                                alt={product.title}
+                                className={styles.list__img}
+                            />
+                            <div className={styles.list__star}>
+                                <FaStar />
+                            </div>
+                            <p className={styles.list__category}>{product.category}</p>
+                            <h2 className={styles.list__title}>{product.title}</h2>
+                            <button type="submit" className={styles.list__btn}>
+                                Add to Cart:
+                            </button>
+                        </div>
+                    )
                 })}
             </div>
         </div>

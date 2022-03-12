a0961d334dd7eb7d3ee31b18e4864d558b594fae
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-03-12 22:53:32 +0545

    feat(component): CategoryItems created

---
+++ app/components/modules/CategoryItems/CategoryItems.jsx
@@ -0,0 +1,13 @@
+import styles from "./CategoryItems.module.scss";
+
+const CategoryItems = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.conatiner__bg}>
+        
+      </div>
+    </div>
+  )
+}
+
+export default CategoryItems;

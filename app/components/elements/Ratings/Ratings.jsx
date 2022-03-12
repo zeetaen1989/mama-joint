15ec7ae04f67bd60676986e42d063015ae1579d4
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-03-12 17:56:21 +0545

    feat(component): Ratings component added

---
+++ app/components/elements/Ratings/Ratings.jsx
@@ -0,0 +1,56 @@
+import { FaRegStar, FaStar, FaStarHalfAlt } from "react-icons/fa";
+import styles from "./Ratings.module.scss";
+
+const Ratings = ({ rating }) => {
+  return (
+    <div className={styles.product__rate}>
+      <span>
+        {rating >= 1 ? (
+          <FaStar />
+        ) : rating >= 0.5 ? (
+          <FaStarHalfAlt />
+        ) : (
+          <FaRegStar />
+        )}
+      </span>
+      <span>
+        {rating >= 2 ? (
+          <FaStar />
+        ) : rating >= 1.5 ? (
+          <FaStarHalfAlt />
+        ) : (
+          <FaRegStar />
+        )}
+      </span>
+      <span>
+        {rating >= 3 ? (
+          <FaStar />
+        ) : rating >= 2.5 ? (
+          <FaStarHalfAlt />
+        ) : (
+          <FaRegStar />
+        )}
+      </span>
+      <span>
+        {rating >= 4 ? (
+          <FaStar />
+        ) : rating >= 3.5 ? (
+          <FaStarHalfAlt />
+        ) : (
+          <FaRegStar />
+        )}
+      </span>
+      <span>
+        {rating >= 5 ? (
+          <FaStar />
+        ) : rating >= 4.5 ? (
+          <FaStarHalfAlt />
+        ) : (
+          <FaRegStar />
+        )}
+      </span>
+    </div>
+  );
+};
+
+export default Ratings;

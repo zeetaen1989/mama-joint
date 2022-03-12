c6691c66ffcb19c5bb7bf7799a01274c8a617441
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-03-12 18:28:40 +0545

    feat(directory): new directory created

---
+++ app/components/modules/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import styles from "./Footer.module.scss";
+
+const Footer = () => {
+    return (
+		<>
+			<div className={styles.footer}>
+				This is a footer
+			</div>
+		</>
+	);
+};
+
+export default Footer;

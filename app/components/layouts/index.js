f9319e252a30fd8abd3e84dc6afa0dd47eb3796d
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-03-12 18:17:53 +0545

    feat(export): new components exported

--- app/components/layouts/index.js
+++ app/components/layouts/index.js
@@ -1,5 +1,2 @@
 export { default as Nav } from "./Nav/Nav";
-export { default as Hero } from "./Hero/Hero";
-export { default as PopularProducts } from "./PopularProducts/PopularProducts";
-export { default as ProductType } from "./ProductType/ProductType";
 export { default as Footer } from "./Footer/Footer";

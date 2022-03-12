a61e8dfd5b5f7e7882a90a3e3ce4e8b92d82d861
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-03-12 16:44:05 +0545

    feat(export): ProductType component exported

--- app/components/layouts/index.js
+++ app/components/layouts/index.js
@@ -1,4 +1,5 @@
 export { default as Nav } from "./Nav/Nav";
 export { default as Hero } from "./Hero/Hero";
 export { default as PopularProducts } from "./PopularProducts/PopularProducts";
+export { default as ProductType } from "./ProductType/ProductType";
 export { default as Footer } from "./Footer/Footer";

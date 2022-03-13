8e5b1064099ecf320bf7d1f18cee0c1c2b2ea089
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-03-13 20:14:26 +0545

    feat(export): components exported

--- app/components/templates/index.js
+++ app/components/templates/index.js
@@ -1,2 +1,3 @@
 export { default as Hero } from "./Hero/Hero";
 export { default as PopularProducts } from "./PopularProducts/PopularProducts";
+export { default as PopularMenu } from "./PopularMenu/PopularMenu";

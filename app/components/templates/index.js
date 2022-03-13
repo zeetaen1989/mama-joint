3cacfe7d644ca41ca5482deb1291bda7911be8fd
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-03-14 00:48:37 +0545

    feat(export): components exported

--- app/components/templates/index.js
+++ app/components/templates/index.js
@@ -3,3 +3,4 @@ export { default as PopularProducts } from "./PopularProducts/PopularProducts";
 export { default as PopularMenu } from "./PopularMenu/PopularMenu";
 export { default as MobileApp } from "./MobileApp/MobileApp";
 export { default as Testimonials } from "./Testimonials/Testimonials";
+export { default as Discounts } from "./Discounts/Discounts";

62a5c5386af166975c3d86692e1a4e83578cd4f5
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-03-15 14:02:56 +0545

    feat(next components): next Image and Link component's markup added

--- app/components/modules/CategoryItems/CategoryItems.jsx
+++ app/components/modules/CategoryItems/CategoryItems.jsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+import Link from "next/link";
 import styles from "./CategoryItems.module.scss";
 
 const CategoryItems = ({ tab, setTab }) => {
@@ -5,9 +7,46 @@ const CategoryItems = ({ tab, setTab }) => {
     <div className={styles.container}>
       <div className={styles.container__subtitle}>
         <p>Faster Food Delivery Service</p>
+        <Image
+          src="/images/delivery.png"
+          alt="Delivery"
+          width={25}
+          height={25}
+        />
+      </div>
+      <div className={styles.container__info}>
+        <h1>Get Delivered while it is still hot.</h1>
+        <p>
+          Bring together your discussions memberships and content. Start your
+          7-day free trial.
+        </p>
+        <div className={styles.info__btn}>
+          <Link href="/menu" passHref >
+            Get Started
+          </Link>
+        </div>
+      </div>
+      <div className={styles.container__img}>
+        <div className={styles.img__leaf}>
+          <Image src="/images/leaf2.png" alt="Leaf" height={35} width={60} />
+        </div>
+        <div className={styles.img__beans}>
+          <Image src="/images/beans.png" alt="Beans" width={180} height={100} />
+        </div>
+        <div className={styles.img__coffee}>
+          <Image
+            src="/images/coffee.png"
+            alt="Coffee"
+            width={180}
+            height={180}
+          />
+        </div>
+        <div className={styles.img__decor}>
+          <Image src="/images/decor.png" alt="Decor" width={70} height={70} />
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default CategoryItems;

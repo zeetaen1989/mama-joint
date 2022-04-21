d7b50a184f4e9f647bed65427f289a9c41fb6fc4
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-04-21 21:21:10 +0545

    feat(slider): slider effect added to testimonials section

--- components/templates/Testimonials/Testimonials.jsx
+++ components/templates/Testimonials/Testimonials.jsx
@@ -1,9 +1,34 @@
+import { useState, useEffect } from "react";
 import Image from "next/image";
 import { testimonial } from "../../../data/testimonials";
 import { Ratings } from "../../elements";
 import styles from "./Testimonials.module.scss";
 
 const Testimonials = () => {
+  const [index, setIndex] = useState(0);
+  const [data, setData] = useState(testimonial);
+
+  useEffect(() => {
+    const lastIndex = data.length - 1;
+
+    if (index < 0) {
+      setIndex(lastIndex);
+    }
+
+    if (index > lastIndex) {
+      setIndex(0);
+    }
+  }, [index, data]);
+
+  useEffect(() => {
+    let slider = setInterval(() => {
+      setIndex(index + 1);
+    }, 5000);
+    return () => {
+      clearInterval(slider);
+    };
+  }, [index]);
+
   return (
     <div className={styles.container}>
       <section className={styles.container__header}>
@@ -14,20 +39,37 @@ const Testimonials = () => {
         </figure>
       </section>
       <section className={styles.container__card}>
-        {testimonial.map((item) => {
+        {testimonial.map((item, itemIndex) => {
+          const { id, img, ratings, location, name, designation, message } =
+            item;
+
+          let cards = "next__slide";
+
+          if (itemIndex === index) {
+            cards = "active__slide";
+          }
+          if (
+            itemIndex === index - 1 ||
+            (index === 0 && itemIndex === item.length - 1)
+          ) {
+            cards = "last__slide";
+          }
+
           return (
-            <div className={styles.cards} key={item.id}>
+            <article className={styles.cards} key={id}>
               <figure className={styles.card__img}>
-                <Image src={item.img} alt={item.name} height={300} width={300} />
+                <Image src={img} alt={name} height={300} width={300} />
               </figure>
-              <section className={styles.card__info}>
-                <h3>{item.name}</h3>
-                <h5>{item.designation}</h5>
-                <span>{item.location}</span>
-                <Ratings rating={item.ratings} />
-                <p><small>{item.message}</small></p>
-              </section>
-            </div>
+              <div className={styles.card__info}>
+                <h3>{name}</h3>
+                <h5>{designation}</h5>
+                <span>{location}</span>
+                <Ratings rating={ratings} />
+                <p>
+                  <small>{message}</small>
+                </p>
+              </div>
+            </article>
           );
         })}
       </section>

adbb49dd0a1c0a9a2a8e54b4eb61b9b06ec8a05e
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-04-21 21:22:12 +0545

    feat(data): new data added to testimonials data

--- data/testimonials.js
+++ data/testimonials.js
@@ -4,9 +4,10 @@ export const testimonial = [
     img: "/images/jane doe.png",
     ratings: 4.5,
     location: "New York",
-    name: 'Jane Doe',
-    designation: 'CEO, XYZ Company',
-    message: "“Him rendered may attended concerns jennings reserved now. Sympathize did now preference unpleasing mrs few. Mrs for hour game room want are fond dare. For detract charmed add talking age. Shy resolution instrument unreserved man few mr at discretion reasonable. Age out full gate bed day lose.”",
+    name: "Jane Doe",
+    designation: "CEO, XYZ Company",
+    message:
+      "“Him rendered may attended concerns jennings reserved now. Sympathize did now preference unpleasing mrs few. Mrs for hour game room want are fond dare. For detract charmed add talking age. Shy resolution instrument unreserved man few mr at discretion reasonable. Age out full gate bed day lose.”",
   },
   {
     id: 2,
@@ -15,6 +16,27 @@ export const testimonial = [
     location: "California",
     name: "John Doe",
     designation: "Senior Executive, ABC Company",
-    message: "“Out believe has request not how comfort evident. Up delight cousins we feeling minutes. Genius has looked end piqued spring. Down has rose feel find man. Learning day desirous informed expenses material returned six the. She enabled invited exposed him another. Reasonably conviction solicitude me mr at discretion reasonable. Age out full gate bed day lose.”"
+    message:
+      "“Out believe has request not how comfort evident. Up delight cousins we feeling minutes. Genius has looked end piqued spring. Down has rose feel find man. Learning day desirous informed expenses material returned six the. She enabled invited exposed him another. Reasonably conviction solicitude me mr at discretion reasonable. Age out full gate bed day lose.”",
   },
-]
+  {
+    id: 3,
+    img: "/images/jane doe.png",
+    ratings: 4.5,
+    location: "New York",
+    name: "Jane Doe",
+    designation: "CEO, XYZ Company",
+    message:
+      "“Him rendered may attended concerns jennings reserved now. Sympathize did now preference unpleasing mrs few. Mrs for hour game room want are fond dare. For detract charmed add talking age. Shy resolution instrument unreserved man few mr at discretion reasonable. Age out full gate bed day lose.”",
+  },
+  {
+    id: 4,
+    img: "/images/john doe.png",
+    ratings: 5,
+    location: "California",
+    name: "John Doe",
+    designation: "Senior Executive, ABC Company",
+    message:
+      "“Out believe has request not how comfort evident. Up delight cousins we feeling minutes. Genius has looked end piqued spring. Down has rose feel find man. Learning day desirous informed expenses material returned six the. She enabled invited exposed him another. Reasonably conviction solicitude me mr at discretion reasonable. Age out full gate bed day lose.”",
+  },
+];

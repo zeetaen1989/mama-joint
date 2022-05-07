451f21753327a0a70530cc41b8cba01e0968e7a5
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-05-07 11:18:28 +0545

    feat(about page): markups for about us page added

--- components/layouts/About/About.jsx
+++ components/layouts/About/About.jsx
@@ -3,9 +3,38 @@ import styles from "./About.module.scss";
 const About = () => {
   return (
     <div className={styles.container}>
-      <h1>About Us</h1>
+      <h1>Our Story</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab, alias
+        necessitatibus libero accusantium facere quaerat quo quidem porro sed
+        neque dolore tempore reiciendis quas est hic, accusamus corporis. Modi,
+        odio. Saepe deserunt, autem fugit molestias enim nostrum cum
+        consectetur, esse dolorem quibusdam dolor quos veritatis facilis
+        similique delectus nemo beatae officiis accusamus suscipit! Perspiciatis
+        deleniti consequatur mollitia architecto inventore dolorem. Amet
+        architecto molestiae, ab tenetur distinctio placeat pariatur repellendus
+        nesciunt praesentium reiciendis alias totam eligendi doloremque. Ipsum
+        quas corrupti laudantium neque molestias magnam assumenda ducimus nobis
+        ullam commodi, esse accusamus? Harum aspernatur saepe doloribus sed ut
+        mollitia quidem ipsum corporis vero debitis inventore nisi earum,
+        obcaecati corrupti quibusdam magni, esse dolore? In, rerum. Saepe
+        voluptates dicta fugiat? Officiis, quibusdam neque. Quod, error cum
+        suscipit at aut perferendis. Similique facilis sint maiores iure culpa
+        numquam quasi tempore consequatur dignissimos, sit in explicabo tenetur
+        autem voluptatem exercitationem suscipit nostrum fuga officia
+        praesentium! Minus tenetur fugit cumque temporibus officia corrupti
+        dolore quod expedita quasi illo ipsam fuga ullam, facere vel soluta, id
+        libero sed non perspiciatis labore facilis saepe accusamus? Beatae,
+        asperiores neque. Odio exercitationem, iste corporis maxime repellendus
+        accusantium enim itaque molestiae quia quasi voluptatibus, fugit, animi
+        porro. Inventore pariatur debitis asperiores id enim amet laboriosam
+        quam aperiam! Ut inventore molestias cupiditate? Quas enim id, nesciunt
+        quia illo autem illum sapiente? Ea, laudantium porro. Expedita
+        reiciendis vero, harum voluptates tenetur minima eligendi velit impedit
+        accusamus nesciunt a iste. Repellat deleniti facilis voluptatum.
+      </p>
     </div>
-  )
-}
+  );
+};
 
 export default About;
